9d40a7ffbeed3f13cb5e6551477a46b7b3cdae72
ALauraOliva <andrea_carol00@hotmail.com>
2023-08-04 15:00:06 -0500

    Done for deploy

--- client/src/components/PreLoader/PreLoader.jsx
+++ client/src/components/PreLoader/PreLoader.jsx
@@ -2,6 +2,8 @@ import stylePreLoader from './PreLoader.module.css'
 
 export const PreLoader = () => {
   return (
-    <img className={stylePreLoader.img} src={require('../../Images/Pikachu_Caminando.a4704ab5.gif')} alt="loading..." />
+    <div className={stylePreLoader.containerPre}>
+      <img className={stylePreLoader.img} src={require('../../Images/Pikachu_Caminando.a4704ab5.gif')} alt="loading..." />
+    </div>
   )
 }
