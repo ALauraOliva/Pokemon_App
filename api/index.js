f4245e6c7bc8d90a514269360157186a9a41f27e
ALauraOliva <andrea_carol00@hotmail.com>
2023-08-04 21:41:20 -0500

    Updating config render

--- api/index.js
+++ api/index.js
@@ -21,7 +21,6 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js'); //?este es mi conexion sequelize <<
 const port = process.env.PORT || 3001;
 
-// Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   console.log('1. DB super conectada')
   
